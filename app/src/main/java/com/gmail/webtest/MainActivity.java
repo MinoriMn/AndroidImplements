d57c0d842e9a7ba8f9abbd2d30a5759ace2a8c76
MinoriMn <lucky_bluebrowneye10280915@au.com>
2019-05-18 15:36:59 +0900

    init PJ

---
+++ app/src/main/java/com/gmail/webtest/MainActivity.java
@@ -0,0 +1,65 @@
+package com.gmail.webtest;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.gmail.webtest.fragment.WebViewFragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        ((NavigationView)findViewById(R.id.nav_view)).setNavigationItemSelectedListener(this);
+
+        fragmentTransaction(new WebViewFragment());
+    }
+
+    private void fragmentTransaction(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+        switch (menuItem.getItemId()){
+            case R.id.navigation_web_view:
+                fragmentTransaction(new WebViewFragment());
+                break;
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/gmail/webtest/fragment/WebViewFragment.java
@@ -0,0 +1,30 @@
+package com.gmail.webtest.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.gmail.webtest.R;
+
+public class WebViewFragment extends Fragment {
+    private WebView webView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        webView = (WebView)view.findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://minorimn.github.io");
+        return view;
+    }
+}
