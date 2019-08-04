3aee943ab6a95fb58f686d890d0711d1845e9c39
MinoriMn <lucky_bluebrowneye10280915@au.com>
2019-08-04 12:16:50 +0900

    realm did sync

--- app/src/main/java/com/gmail/webtest/activity/RealmMemoActivity.java
+++ app/src/main/java/com/gmail/webtest/activity/RealmMemoActivity.java
@@ -2,13 +2,21 @@ package com.gmail.webtest.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.gmail.webtest.R;
 
-public class RealmMemoActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class RealmMemoActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String EXTRA_KEY_IS_NEW = "EXTRA_KEY_IS_NEW";
     private boolean isNew = false;
+
+    private EditText titleEditText, contentEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +25,23 @@ public class RealmMemoActivity extends AppCompatActivity {
 
         isNew = getIntent().getBooleanExtra(EXTRA_KEY_IS_NEW, false);
 
+        titleEditText = findViewById(R.id.memo_title);
+        contentEditText = findViewById(R.id.memo_content);
+
         if(isNew){
             ((Button)findViewById(R.id.memo_create_button)).setText("作成");
         }else{
             ((Button)findViewById(R.id.memo_create_button)).setText("更新");
         }
+        findViewById(R.id.memo_create_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String title = titleEditText.getText().toString();
+        String content = contentEditText.getText().toString();
+
+        Date date = new Date();
+        String dateString = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.JAPANESE).format(date);
     }
 }
--- app/src/main/java/com/gmail/webtest/fragment/RealmMemoListFragment.java
+++ app/src/main/java/com/gmail/webtest/fragment/RealmMemoListFragment.java
@@ -1,21 +1,36 @@
 package com.gmail.webtest.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gmail.webtest.R;
+import com.gmail.webtest.activity.RealmMemoActivity;
 
 public class RealmMemoListFragment extends Fragment {
+    private static final int REQUEST_NEW_MEMO = 100;
+    private static final int REQUEST_MEMO_EDIT = 101;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = inflater.inflate(R.layout.fragment_realm_memo_list, container, false);
+
+        ((FloatingActionButton)view.findViewById(R.id.new_memo_fab)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra(RealmMemoActivity.EXTRA_KEY_IS_NEW, true);
+                startActivityForResult(intent, REQUEST_NEW_MEMO);
+            }
+        });
 
         return view;
     }
